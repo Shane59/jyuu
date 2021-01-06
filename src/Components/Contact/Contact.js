31f9294e9d436c66468e6df3fa76f954cab441df
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2021-01-06 20:39:21 +0900

    add profile and contact

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Contact(props) {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc4G5FhAkYMPtFtGBzd3v4bFlrVBTUCBt_AW4d1JwKqW9dlmw/viewform?embedded=true" width="640" height="980" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+    </div>
+  )
+}
