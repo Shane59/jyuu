e6b765d05ec4091f9f3c5513938e2f653b422061
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2021-01-10 17:34:56 +0900

    create work page

---
+++ src/Components/Works/Solution/Solution.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SolutionWrapper = styled.div`
+  display: flex;
+  width: 900px;
+  margin: 0 auto;
+  background-image: url(${props => props.image});
+  background-size: 420px auto;
+  background-repeat: no-repeat;
+`;
+const DescriptionWrapper = styled.div`
+  flex: 0 1 400px;
+  margin-top: 310px;
+`;
+const FeeWrapper = styled.div`
+  flex: 0 1 400px;
+  height: 390px;
+  padding: 24px;
+  background-color: rgba(0, 0, 0, 0.1);
+  margin-top: 40px;
+  z-index: -1;
+`;
+const SolutionNumber = styled.h2`
+  display: inline;
+`;
+
+
+//props
+
+export default function Solution(props) {
+  return (
+    <>
+      <h1>{props.title}</h1>
+      <SolutionWrapper
+        image={props.image}>
+        <DescriptionWrapper>
+          <SolutionNumber>{props.num}.</SolutionNumber>{props.description}
+        </DescriptionWrapper>
+        <FeeWrapper>
+          {props.fee}
+        </FeeWrapper>
+      </SolutionWrapper>
+    </>
+  )
+}
