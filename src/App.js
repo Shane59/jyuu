c1463e89a994ca9ed9142766d41ec51fba5e838e
Shinya Aoi <saoi@Shinyas-MacBook-Pro.local>
2021-04-06 08:17:33 +0900

    ga edit 4

--- src/App.js
+++ src/App.js
@@ -2,30 +2,20 @@
 import Maintenance from './Components/MaintenancePage/MaintenancePage';
 import React, { useEffect } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Works from './Components/Works/Works';
 import Top from './Components/Top/Top';
+
 import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
-import { initGA, GApageView } from './index';
-
-// const TRACKING_ID = "G-Y4W59DCDL1";
-
-// ReactGA.initialize(TRACKING_ID);
-
-// const history = createBrowserHistory();
-
-// Initialize google analytics page view tracking
-// history.listen(location => {
-//   console.log('page history');
-  
-//   ReactGA.set({ page: location.pathname });
-//   ReactGA.pageview('/');
-// });
 
 function App() {
-  // useEffect(() => { initGA(); }, []);
-  // useEffect(() => { GApageView("landing"); }, []);
+  useEffect(() => {
+    ReactGA.initialize('G-Y4W59DCDL1');
+
+    // to report page view
+    ReactGA.pageview('/');
+  });
+  
   return (
     <Router>
       <div className="App">
